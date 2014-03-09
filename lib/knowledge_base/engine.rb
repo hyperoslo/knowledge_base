763554f7dfe9b227ff9ae895ffd1ccc8f6548415
Johannes Gorset <jgorset@gmail.com>
2014-03-09 22:06:16 +0100

    Load decorators from client applications

--- lib/knowledge_base/engine.rb
+++ lib/knowledge_base/engine.rb
@@ -5,6 +5,12 @@ module KnowledgeBase
   class Engine < ::Rails::Engine
     isolate_namespace KnowledgeBase
 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
