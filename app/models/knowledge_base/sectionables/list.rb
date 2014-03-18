1a704912f099a4b31ba872a9960e6949506e46ec
Johannes Gorset <jgorset@gmail.com>
2014-03-18 11:28:35 +0100

    Accept nested attributes

--- app/models/knowledge_base/sectionables/list.rb
+++ app/models/knowledge_base/sectionables/list.rb
@@ -2,6 +2,8 @@ module KnowledgeBase::Sectionables
   class List < Sectionable
     has_many :items, -> { order 'position ASC' }
 
+    accepts_nested_attributes_for :items, allow_destroy: true
+
     def to_s
       "#{super}: #{title}"
     end
