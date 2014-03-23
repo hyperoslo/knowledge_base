60929089c6d70f4e53fd905c83690c46f359db96
Johannes Gorset <jgorset@gmail.com>
2014-03-23 11:34:15 +0100

    Order images ascending

--- app/models/knowledge_base/sectionables/gallery.rb
+++ app/models/knowledge_base/sectionables/gallery.rb
@@ -1,6 +1,6 @@
 module KnowledgeBase::Sectionables
   class Gallery < Sectionable
-    has_many :images, -> { order 'position DESC' }
+    has_many :images, -> { order 'position ASC' }
 
     accepts_nested_attributes_for :images, allow_destroy: true
 
