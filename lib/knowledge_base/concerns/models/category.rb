e0d465471f72d08e329a0bbb43ba94241011a240
Johannes Gorset <jgorset@gmail.com>
2014-03-24 23:07:11 +0100

    Sort category article associations by position

--- lib/knowledge_base/concerns/models/category.rb
+++ lib/knowledge_base/concerns/models/category.rb
@@ -9,7 +9,7 @@ module KnowledgeBase::Concerns::Models::Category
     friendly_id :title, use: :slugged
 
     belongs_to :category
-    has_many :category_article_associations
+    has_many :category_article_associations, -> { order 'position ASC' }
     has_many :articles, through: :category_article_associations
 
     accepts_nested_attributes_for :category_article_associations, allow_destroy: true
