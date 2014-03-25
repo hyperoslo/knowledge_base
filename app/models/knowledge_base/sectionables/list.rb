067d475b051344bcb06f941de55799c6e3eabc44
Johannes Gorset <jgorset@gmail.com>
2014-03-25 09:40:53 +0100

    Implement List#image

--- app/models/knowledge_base/sectionables/list.rb
+++ app/models/knowledge_base/sectionables/list.rb
@@ -1,5 +1,7 @@
 module KnowledgeBase::Sectionables
   class List < Sectionable
+    mount_uploader :image, KnowledgeBase::ImageUploader
+
     has_many :items, -> { order 'position ASC' }
 
     accepts_nested_attributes_for :items, allow_destroy: true
