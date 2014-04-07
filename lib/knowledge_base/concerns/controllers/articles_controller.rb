0fa4779816b21affb8e44c7a401f52bb548d0cc0
Johannes Gorset <jgorset@gmail.com>
2014-04-07 15:07:49 +0200

    Set @category in articles#show

--- lib/knowledge_base/concerns/controllers/articles_controller.rb
+++ lib/knowledge_base/concerns/controllers/articles_controller.rb
@@ -3,8 +3,16 @@ module KnowledgeBase
     extend ActiveSupport::Concern
 
     included do
+      before_action :set_category
+
       def show
         @article = Article.published.friendly.find params[:id]
+      end
+
+      private
+
+      def set_category
+        @category = Category.friendly.find params[:category_id] if params.has_key? :category_id
       end
     end
   end
