7e7286bc1e8d1e22ce0cb354564247b29e5f3960
Johannes Gorset <jgorset@gmail.com>
2014-04-07 15:30:25 +0200

    Seed published articles

--- lib/knowledge_base/seeds.rb
+++ lib/knowledge_base/seeds.rb
@@ -7,7 +7,7 @@ def fixture file
 end
 
 category    = KnowledgeBase::Category.create! title: Faker::Lorem.words.join, description: Faker::Lorem.sentence
-article     = KnowledgeBase::Article.create! title: Faker::Lorem.words.join, description: Faker::Lorem.sentence
+article     = KnowledgeBase::Article.create! title: Faker::Lorem.words.join, description: Faker::Lorem.sentence, published_at: Time.now
 association = KnowledgeBase::CategoryArticleAssociation.create! category: category, article: article
 
 text    = KnowledgeBase::Sectionables::Text.create! heading: Faker::Lorem.words.join, lead: Faker::Lorem.sentence, body: Faker::Lorem.paragraph
