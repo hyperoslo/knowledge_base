95fd51af5c0d1a61d5ae399a59bd4de9a2587c46
Sindre Moen <sindrenm@gmail.com>
2014-04-10 23:53:42 +0200

    Rename migration names that were too long
    Bundler couldn't even install the stuff. Fun stuff!

--- db/migrate/20140410203342_rename_attachment_lists_to_attachments_lists.rb
+++ db/migrate/20140410203342_rename_attachment_lists_to_attachments_lists.rb
@@ -1,4 +1,4 @@
-class RenameKnowledgeBaseSectionablesAttachmentListsToKnowledgeBaseSectionablesAttachmentsLists < ActiveRecord::Migration
+class RenameAttachmentListsToAttachmentsLists < ActiveRecord::Migration
   def change
     rename_table :knowledge_base_sectionables_attachment_lists, :knowledge_base_sectionables_attachments_lists
   end
--- db/migrate/20140410205056_rename_attachments_to_attachments_attachments.rb
+++ db/migrate/20140410205056_rename_attachments_to_attachments_attachments.rb
@@ -1,4 +1,4 @@
-class RenameKnowledgeBaseSectionablesAttachmentsToKnowledgeBaseSectionablesAttachmentsAttachments < ActiveRecord::Migration
+class RenameAttachmentsToAttachmentsAttachments < ActiveRecord::Migration
   def change
     rename_table :knowledge_base_sectionables_attachments, :knowledge_base_sectionables_attachments_attachments
   end
