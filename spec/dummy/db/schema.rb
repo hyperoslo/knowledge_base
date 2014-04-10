f1ffabeec771a74c477db8298c9e200ce2351b90
Sindre Moen <sindrenm@gmail.com>
2014-04-11 01:19:48 +0200

    Fix sectionable_types after renaming models

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410224403) do
+ActiveRecord::Schema.define(version: 20140410231433) do
 
   create_table "knowledge_base_articles", force: true do |t|
     t.string   "title"
