b81a8b69c067544bda81e95071deeb76b6db93ed
Johannes Gorset <jgorset@gmail.com>
2014-04-30 14:14:42 +0200

    Fix generators after refactoring links and introducing attachments
    The 'Links' is now Links::List, and attachments are now organized in
    'Attachment::List'.

---
+++ lib/generators/knowledge_base/active_admin/templates/sectionables/attachments/list.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register KnowledgeBase::Sectionables::Attachments::List do
+  menu parent: I18n.t('activerecord.models.knowledge_base/section/kind.other')
+
+  index do
+    selectable_column
+    
+    column :links do |list|
+      list.attachments.map(&:title).join ", "
+    end
+
+    actions
+  end
+
+  form do |f|
+    f.has_many :attachments do |f|
+      f.input :title
+      f.input :file
+      f.input :description
+    end
+
+    f.actions
+  end
+end
