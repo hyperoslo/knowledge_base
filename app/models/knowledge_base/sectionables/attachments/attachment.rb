020e19d28da555bd9228a5f1f5f4fd982fca2a57
Sindre Moen <sindrenm@gmail.com>
2014-05-14 13:24:59 +0200

    Validate title, file and list on attachments

--- app/models/knowledge_base/sectionables/attachments/attachment.rb
+++ app/models/knowledge_base/sectionables/attachments/attachment.rb
@@ -3,5 +3,7 @@ module KnowledgeBase::Sectionables::Attachments
     belongs_to :list, foreign_key: :attachment_list_id
 
     mount_uploader :file, KnowledgeBase::GenericFileUploader
+
+    validates :title, :file, :list, presence: true
   end
 end
--- app/models/knowledge_base/sectionables/attachments/list.rb
+++ app/models/knowledge_base/sectionables/attachments/list.rb
@@ -2,7 +2,8 @@ module KnowledgeBase::Sectionables::Attachments
   class List < ::KnowledgeBase::Sectionables::Sectionable
     has_many :sections, as: :sectionable, dependent: :destroy
 
-    has_many :attachments, dependent: :destroy, foreign_key: :attachment_list_id
+    has_many :attachments, dependent: :destroy, inverse_of: :list,
+      foreign_key: :attachment_list_id
     accepts_nested_attributes_for :attachments, allow_destroy: true
 
     def to_s
