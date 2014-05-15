73961d5173e4cefdc7793d6aa891db058e151426
Johannes Gorset <jgorset@gmail.com>
2014-05-15 14:27:02 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 KnowledgeBase::Engine.routes.draw do
+  resources :articles, path: KnowledgeBase.config.articles_path, only: [:show]
+
   resources :categories, path: KnowledgeBase.config.categories_path, only: [:index, :show] do
-    resources :articles, path: KnowledgeBase.config.articles_path, only: [:show], shallow: true
     resources :articles, path: KnowledgeBase.config.articles_path, only: [:show]
   end
 end
