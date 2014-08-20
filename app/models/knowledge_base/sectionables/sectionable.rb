75c922a1bde70c4a824cd4dc2884578f302c76e4
Sindre Moen <sindrenm@gmail.com>
2014-08-20 20:32:59 +0200

    Enumerate on sectionables' custom_style

--- app/models/knowledge_base/sectionables/sectionable.rb
+++ app/models/knowledge_base/sectionables/sectionable.rb
@@ -1,5 +1,14 @@
 module KnowledgeBase::Sectionables
   class Sectionable < ActiveRecord::Base
+    extend Enumerize
+
+    def self.inherited(child)
+      styles = KnowledgeBase.config.custom_section_styles[child.name] || [ ]
+      child.enumerize :custom_style, in: styles
+
+      super
+    end
+
     self.abstract_class = true
 
     def to_s
